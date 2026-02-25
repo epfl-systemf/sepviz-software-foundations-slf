e08c53ac759029abaea725753cccd6d4851500db
Yawen Guan <yawen.guan@proton.me>
2026-02-25 18:28:50 +0100

    repr: Display MList_cons

--- Repr.v
+++ Repr.v
@@ -137,10 +137,6 @@ Notation "p '~>' 'Record3'' f1 x1 f2 x2 f3 x3" :=
     case. *)
 
 
-(*|
-.. coq:: none
-|*)
-
 Lemma MList_nil : forall p,
   (MList nil p) = \[p = null].
 Proof using. auto. Qed.
@@ -149,8 +145,6 @@ Lemma MList_cons : forall p x L',
   MList (x::L') p =
   \exists q, (p ~~~> `{ head := x; tail := q}) \* (MList L' q).
 Proof using. auto. Qed.
-
-(*||*)
 
 (** In addition, it is also very useful in proofs to reformulate the definition
     of [MList L p] in the form of a case analysis on whether the pointer [p] is
