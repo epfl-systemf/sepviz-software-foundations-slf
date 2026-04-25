a304a40cd1af5f4414596e047665e7d0dea563af
Yawen Guan <yawen.guan@proton.me>
2026-04-25 14:09:51 +0200

    nix flake: Use coq 8.20

--- flake.nix
+++ flake.nix
@@ -1,14 +1,20 @@
 {
   description = "A Nix-flake-based SLF development environment";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    alectryon-src = {
+      url = "github:cpitclaudel/alectryon/v2.0.0";
+      flake = false;
+    };
   };
   outputs =
     {
       self,
       nixpkgs,
       flake-utils,
+      alectryon-src,
     }:
     flake-utils.lib.eachSystem
       [
@@ -20,18 +26,46 @@
       (
         system:
         let
-          pkgs = import nixpkgs { inherit system; };
-          coqPkgs = pkgs.coqPackages_8_18;
+          overlay = final: prev: {
+            alectryon = final.python312Packages.buildPythonPackage {
+              pname = "alectryon";
+              version = "2.0.0";
+              src = alectryon-src;
+              format = "pyproject";
+              nativeBuildInputs = with final.python312Packages; [
+                setuptools
+              ];
+              propagatedBuildInputs = with final.python312Packages; [
+                pygments
+                docutils
+                sphinx
+                dominate
+                beautifulsoup4
+                myst-parser
+              ];
+              postPatch = ''
+                substituteInPlace alectryon/serapi.py \
+                  --replace "'pp_depth': 30" "'pp_depth': 100" \
+                  --replace "'pp_margin': 55" "'pp_margin': 100"
+              '';
+              doCheck = false;
+            };
+            vsrocq-language-server-8_20 = final.rocqPackages.vsrocq-language-server.override {
+              coq = final.coqPackages_8_20.coq;
+            };
+          };
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ overlay ];
+          };
         in
         {
           devShells.default = pkgs.mkShell {
-            packages =
-              with pkgs;
-              [ python312Packages.alectryon ]
-              ++ (with coqPkgs; [
-                coq
-                serapi # required by alectryon
-              ]);
+            packages = with pkgs; [
+              alectryon
+              vsrocq-language-server-8_20
+              coqPackages_8_20.coq
+            ];
           };
         }
       );
